728ece781ba0eb5e590bc88cb03a3a04adf2d216
phileasFoggy22 <phileasFoggy@gmail.com>
2019-06-04 15:13:03 +0100

    branch commit 2 with map

--- src/main/App.java
+++ src/main/App.java
@@ -1,10 +1,22 @@
 package main;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world to the standard out");
-		System.out.println("Hello world to the standard out");
+
+		Map<String, Integer> hm = new HashMap<String, Integer>();
+		hm.put("a", new Integer(100));
+		hm.put("b", new Integer(200));
+		hm.put("c", new Integer(300));
+		hm.put("d", new Integer(400));
+
+		for (String value : hm.keySet()) {
+			System.out.println("The value is " + value);
+		}
 
 	}
 
